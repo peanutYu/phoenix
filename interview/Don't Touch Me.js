7311fedad439e760aefcfa00169cd981496234d5
peanut <287130319@qq.com>
2019-06-04 11:21:07 +0800

    :rocket: 新增Don't touch me

--- interview/Don't Touch Me.js
+++ interview/Don't Touch Me.js
@@ -3,5 +3,15 @@
 // 完成 tomy 这个对象，禁止对 tomy 的内容进行修改（增加、修改、删除）。一旦有人对 tomy 进行任何的修改，都用 console.log 打印 Don't Touch Me.。
 
 const tomy = new Proxy({}, {
-  
+  a: 1,
+  get: function (target, key, receiver) {
+    console.log('Don\'t Touch Me.');
+  },
+  set: function (target, key, value, receiver) {
+    console.log('Don\'t Touch Me.');
+  },
+  deleteProperty: function() {
+    console.log('Don\'t Touch Me.');    
+  },
 });
+
